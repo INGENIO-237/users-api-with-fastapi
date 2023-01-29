b5ce48c64a0906bd3f9bf5392967c6237428f59a
INGENIO <ingeniokhalif@gmail.com>
2023-01-29 15:28:56 +0100

    Modify README and document db.py

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 #   USER-API
 
-This is a sample Users CRUD operations API built with FastAPI
+This is a sample Users CRUD operations API built with FastAPI and MongoDB Database
 
 ![FastAPI_image](fastapi-mongodb.png)
--- config/db.py
+++ config/db.py
@@ -1,3 +1,7 @@
 from pymongo import MongoClient
 
+# Connect to the local instance of mongodb server
 db = MongoClient()
+
+# You can also use a specific mongoDB server by passing its
+# address in the parenthesis of MongoClient() above
